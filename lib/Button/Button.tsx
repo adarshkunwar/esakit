5f99190067e28e95a7bb29dbc519c9f324cc9249
adarshkunwar123 <adarsh.kunwar@wealthlane.co>
2025-10-31 15:16:43 +0545

    feat:initialized

---
+++ lib/Button/Button.tsx
@@ -0,0 +1,11 @@
+import { type ReactNode } from "react"
+
+const Button = ({ children }: { children: ReactNode }) => {
+	return (
+		<button>
+			{children}
+		</button>
+	)
+}
+
+export default Button
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/index.ts'),
+      name: 'esakit',
+      fileName: 'esakit'
+    },
+    rolldownOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime'
+        }
+      }
+    }
+  }
+})
